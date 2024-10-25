f14da05633d36eeaaecaa800850e84f9e321f5be
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-10-25 23:36:58 +0300

    Create MvCommand_2_Test

---
+++ CLIProject/test/cli/commands/MvCommand_2_Test.java
@@ -0,0 +1,50 @@
+package cli.commands;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+// import cli.commands.MvCommand;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MvCommand_2_Test {
+    private MvCommand mvCommand;
+
+    @Before
+    public void setUp() throws IOException {
+        mvCommand = new MvCommand();
+
+        File srcFile = new File("testFile.txt");
+        assertTrue(srcFile.createNewFile());
+
+        File destDir = new File("destinationDir");
+        assertTrue(destDir.mkdir());
+    }
+
+    @After
+    public void tearDown() {
+        new File("testFile.txt").delete();
+        new File("destinationDir/testFile.txt").delete();
+        new File("destinationDir").delete();
+    }
+
+    @Test
+    public void testExecuteMovesFileToDirectory() throws IOException {
+
+        File srcFile = new File("testFile.txt");
+        File destDir = new File("destinationDir");
+        File movedFile = new File(destDir, "testFile.txt");
+        
+        String[] args = {"mv", "testFile.txt", "destinationDir/testFile.txt"};
+        mvCommand.execute(args);
+    
+        
+        assertFalse(srcFile.exists());
+        assertTrue(movedFile.exists());
+    }
+    
+}
