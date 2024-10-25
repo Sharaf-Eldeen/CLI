ab9e17c87fec21a26e85876f39531aa50cff0b87
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-10-25 23:37:08 +0300

    Create MkdirCommandTest

---
+++ CLIProject/test/cli/commands/MkdirCommandTest.java
@@ -0,0 +1,47 @@
+package cli.commands;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+// import cli.commands.*;
+
+
+public class MkdirCommandTest {
+    private MkdirCommand Mkdircommand;
+
+    @Before
+    public void setup() {
+        Mkdircommand = new MkdirCommand();
+    }
+
+    @After
+    public void tearDown() {
+        File testDirectory = new File("testDir");
+        if(testDirectory.exists()) {
+            testDirectory.delete();
+        }
+    }
+
+    @Test
+    public void testExecuteCreatesDirectory() {
+        String[] args = {"mkdir", "testDir"};
+        Mkdircommand.execute(args);
+        File testDirectory = new File("testDir");
+        assertTrue(testDirectory.exists()&&testDirectory.isDirectory());
+    }
+
+    @Test
+    public void testExecuteFailsWithNoArguments() {
+        String[] args = {"mkdir"};
+        Mkdircommand.execute(args);
+        File testDirectory = new File("mkdir");
+        assertFalse(testDirectory.exists()&&testDirectory.isDirectory());
+    }
+
+
+}
