cb84bd73975335ed5de4296a1dc67e1254e305f7
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-10-25 23:36:46 +0300

    Create MvCommandTest

---
+++ CLIProject/test/cli/commands/MvCommandTest.java
@@ -0,0 +1,61 @@
+package cli.commands;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+// import cli.commands.MvCommand;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MvCommandTest {
+    private MvCommand Mvcommand;
+
+    @Before
+    public void setup() throws IOException {
+        Mvcommand = new MvCommand();
+        File srcFile = new File("testFile.txt");
+        assertTrue(srcFile.createNewFile());
+    }
+
+    @After
+    public void tearDown() {
+        File srcFile = new File("testFile.txt");
+        File dstFile = new File("movedFile.txt");
+        srcFile.delete();
+        dstFile.delete();
+        assertFalse(srcFile.exists() || dstFile.exists());
+    }
+
+    @Test
+    public void testExecuteMovesFileSuccessfully() {
+        String[] args = {"mv", "testFile.txt", "movedFile.txt"};
+        Mvcommand.execute(args);
+        File srcFile = new File("testFile.txt");
+        File dstFile = new File("movedFile.txt");
+        assertFalse(srcFile.exists());
+        assertTrue(dstFile.exists());
+    }
+
+    @Test
+    public void testExecuteFailsIfSourceDoesNotExist() {
+        String[] args = {"mv", "nonExistentFile.txt", "movedFile.txt"};
+        Mvcommand.execute(args);
+        File dstFile = new File("movedFile.txt");
+        assertFalse(dstFile.exists());
+    }
+
+    @Test 
+    public void testExecuteFailsWithInsufficientArguments() {
+        String[] args = {"mv", "testFile.txt"};
+        Mvcommand.execute(args);
+        File srcFile = new File("testFile.txt");
+        assertTrue(srcFile.exists());
+    }
+
+    
+}
