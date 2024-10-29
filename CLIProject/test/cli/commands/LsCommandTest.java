c2cfcbc391a80209274c668438bd5556daf70152
Asmaa Atef <asmaatef6789@gmail.com>
2024-10-30 01:27:40 +0300

    Finishing ls j unit

--- CLIProject/test/cli/commands/LsCommandTest.java
+++ CLIProject/test/cli/commands/LsCommandTest.java
@@ -1,69 +1,92 @@
 package cli.commands;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.PrintStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import cli.CommandLineInterpreter;
+
 public class LsCommandTest {
     private LsCommand lsCommand;
-    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
+    private CommandLineInterpreter cli;
+    private Path testDir;
+    private Path hiddenFile;
 
     @Before
-    public void setUp() {
-        System.setOut(new PrintStream(outputStream));
-        lsCommand = new LsCommand();
+    public void setUp() throws IOException {
+        cli = new CommandLineInterpreter();
+        lsCommand = new LsCommand(cli);
+        testDir = Files.createTempDirectory("testDir");
+        cli.setCurrentDirectory(testDir.toString());
+        hiddenFile = Files.createFile(testDir.resolve(".hiddenFile"));
     }
 
     @After
-    public void tearDown() {
-        System.setOut(originalOut);
-        outputStream.reset();
+    public void tearDown() throws IOException {
+        deleteRecursively(testDir.toFile());
+    }
+    private void deleteRecursively(File file) throws IOException {
+        if (file.isDirectory()) {
+            for (File subFile : file.listFiles()) {
+                deleteRecursively(subFile);
+            }
+        }
+        file.delete();
     }
 
+
     @Test
-    public void testExecuteCurrentDirectory() {
-        lsCommand.execute(new String[]{"ls"});
-        String output = outputStream.toString().trim();
-        File currentDir = new File(System.getProperty("user.dir"));
-        assertTrue("Output should list current directory files and folders", output.contains(currentDir.getName()));
+    public void testExecuteCurrentDirectory() throws IOException {
+        Path sampleFile = Files.createFile(testDir.resolve("sampleFile.txt"));
+        String output = lsCommand.execute(new String[]{"ls"});
+        assertTrue("Output should list the files and folders in the current directory", output.contains("sampleFile.txt"));
     }
 
     @Test
     public void testExecuteHiddenFiles() {
-        lsCommand.execute(new String[]{"ls", "-a"});
-        String output = outputStream.toString().trim();
-        assertTrue("Output should contain hidden files when -a flag is used", output.contains(".hiddenFile"));
+        String output = lsCommand.execute(new String[]{"ls", "-a"});
+        assertTrue("Output should include hidden files when '-a' flag is used", output.contains(".hiddenFile"));
     }
 
     @Test
-    public void testExecuteReverseOrder() {
-        lsCommand.execute(new String[]{"ls", "-r"});
-        String output = outputStream.toString().trim();
+    public void testExecuteReverseOrder() throws IOException {
+        Files.createFile(testDir.resolve("Afile"));
+        Files.createFile(testDir.resolve("Bfile"));
+
+        String output = lsCommand.execute(new String[]{"ls", "-r"});
         String[] lines = output.split("\\n");
-        assertTrue("Output should be in reverse alphabetical order", lines[0].compareTo(lines[lines.length - 1]) > 0);
+
+        assertTrue("Output should list files in reverse alphabetical order", 
+                   lines[0].contains("Bfile") && lines[1].contains("Afile"));
     }
 
     @Test
-    public void testExecuteSpecificDirectory() {
-        lsCommand.execute(new String[]{"ls", "src"});
-        String output = outputStream.toString().trim();
-        assertTrue("Output should list files in the specified 'src' directory", output.contains("src"));
+    public void testExecuteSpecificDirectory() throws IOException {
+        Path srcDir = Files.createDirectory(testDir.resolve("src"));
+        assertTrue("The 'src' directory should be created", Files.exists(srcDir));
+
+        Path testFile = Files.createFile(srcDir.resolve("testFile.txt"));
+        assertTrue("The 'testFile.txt' file should be created", Files.exists(testFile));
+
+        String output = lsCommand.execute(new String[]{"ls", srcDir.toAbsolutePath().toString()});
+    
+        assertTrue("Output should list the contents of the specified 'src' directory", output.contains("testFile.txt"));
     }
 
     @Test
     public void testExecuteNonexistentDirectory() {
         String output = lsCommand.execute(new String[]{"ls", "nonexistentDir"});
-        assertEquals("Output should show an error for nonexistent directory",
+        assertEquals("Output should indicate an error for a nonexistent directory",
                 "Cannot access directory: nonexistentDir", output);
     }
 }
+
 
