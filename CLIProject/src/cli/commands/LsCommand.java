f6fdb93ecdd46bdc87e266fa63080f2b75273340
Asmaa Atef <asmaatef6789@gmail.com>
2024-10-31 14:01:20 +0300

    Update LsCommand.java

--- CLIProject/src/cli/commands/LsCommand.java
+++ CLIProject/src/cli/commands/LsCommand.java
@@ -7,38 +7,45 @@ import java.util.Arrays;
 
 public class LsCommand implements cli.Command {
     private CommandLineInterpreter cli;
-
+    // assign cli to the this atribute so i can keep track of the current opened dir over all the calls of the function
     public LsCommand(CommandLineInterpreter cli) {
         this.cli = cli;
     }
 
     @Override
     public String execute(String[] args) {
-        File dir;
+        // first thing checking for the arguments to determine which one i will do from those four 
+        // ( ls   ls+ path    ls -a   ls -r)
+        File currdir;
         if (args.length > 1 && !args[1].startsWith("-")) {
-            dir = new File(args[1]);
+            currdir = new File(args[1]);
+            //here if there is args but not start with - means not ls -a or ls -r   so i will do ls +path and assign curr dir to the given path
         } else {
-            dir = new File(cli.getCurrentDirectory());
+            currdir = new File(cli.getCurrentDirectory());
+            // else the currdir will have the path of the current open dir
         }
-
-        if (!dir.isDirectory()) {
-            return "Cannot access directory: " + dir.getPath();
+        // if the curr dir wrong it will return here and also i make a test for this point
+        if (!currdir.isDirectory()) {
+            return "Cannot access directory: " + currdir.getPath();
         }
-
-        File[] files = dir.listFiles();
-        if (files == null) {
+        // if the curr dir is correct then i will make array(curr files) and get all the files and dirs insead the currdir
+        File[] currfiles = currdir.listFiles();
+        // if the array is empty or null i will terminate the function and also there is a tset for this point
+        if (currfiles == null) {
             return "Cannot access directory contents.";
         }
 
 
-
+        // i will store all file with hidden ones in case the arg was -a  and i will store the files with reversed order incase if ls -r
         boolean all = Arrays.asList(args).contains("-a");
-        boolean reversed = Arrays.asList(args).contains("-r");
+        boolean rev = Arrays.asList(args).contains("-r");
+        
 
+        // as the function type is tring so i make a string to store the output in it
         StringBuilder ret = new StringBuilder();
-        Arrays.stream(files)
+        Arrays.stream(currfiles)
                 .filter(file -> all ||! file.isHidden())
-                .sorted((f1, f2) -> reversed ? f2.getName().compareTo(f1.getName()) : f1.getName().compareTo(f2.getName()))
+                .sorted((f1, f2) -> rev ? f2.getName().compareTo(f1.getName()) : f1.getName().compareTo(f2.getName()))
                 .forEach(file -> {
 
                     if (file.isDirectory()) {
