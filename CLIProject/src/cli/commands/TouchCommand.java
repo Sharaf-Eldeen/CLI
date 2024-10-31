3852f3a0dac89d4c216511f5aa4cdb55ff3a6f07
Asmaa Atef <asmaatef6789@gmail.com>
2024-10-31 14:01:53 +0300

    Update TouchCommand.java

--- CLIProject/src/cli/commands/TouchCommand.java
+++ CLIProject/src/cli/commands/TouchCommand.java
@@ -7,33 +7,38 @@ import java.io.IOException;
 
 public class TouchCommand implements Command {
     private CommandLineInterpreter cli;
-
+    // assign cli to the this atribute so i can keep track of the current opened dir over all the calls of the function
     public TouchCommand(CommandLineInterpreter cli) {
         this.cli = cli;
     }
 
     @Override
     public String execute(String[] args) {
+        // checking for the arg to know if all needed args are given or there is missing(here all needed if file name or a path)
         if (args.length < 2) {
             return "Usage: touch <filename>";
         }
 
-        String fileName = args[1];
-        File file;
-        file = new File(cli.getCurrentDirectory(), fileName);
+
+        // geting the path of the given file name starting from the current dir
+        String del = args[1];
+        File delfile;
+        delfile = new File(cli.getCurrentDirectory(), del);
         try {
-            File parentDir = file.getParentFile();
-            if (parentDir != null && !parentDir.exists()) {
-                if (!parentDir.mkdirs()) {
-                    return "Failed to create directories: " + parentDir.getPath();
+            File parDir = delfile.getParentFile();
+            // checking if the path contains dirs not exist if there is any we will create them and check if the creation done or not
+            if (parDir != null && !parDir.exists()) {
+                if (!parDir.mkdirs()) {
+                    return "Failed to create directories: " + parDir.getPath();
                 }
             }
-
-            if (file.createNewFile()) {
-                return "File created: " + file.getPath();
+            // here we check if we can create the file or not ( we can create it if there is no file in the same dir have the same name else we return that the file is already exist)
+            if (delfile.createNewFile()) {
+                return "File created: " + delfile.getPath();
             } else {
                 return "File already exists.";
             }
+            // here handling the error that might happen
         } catch (IOException e) {
             return "An error occurred: " + e.getMessage();
         }
